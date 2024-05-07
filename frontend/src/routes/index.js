c0060cbe2878df1f81e613b29ee7b9066d48c1da
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2024-05-08 00:59:41 +0530

    Login page completed and started signing up process

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,6 +3,7 @@ import App from "../App"
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import ForgotPassword from "../pages/ForgotPassword";
+import Signup from "../pages/Signup";
 
 const router=createBrowserRouter([
     {
@@ -19,6 +20,10 @@ const router=createBrowserRouter([
         {
         path:"ForgotPassword",
         element:<ForgotPassword/>
+        },
+        {
+            path:"sign-up",
+            element:<Signup/>
         }
     ]
     }
